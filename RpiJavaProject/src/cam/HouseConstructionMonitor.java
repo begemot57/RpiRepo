87a2d4d165effaa7cde15339c470910d918053f4
begemot57 <begemot57@hotmail.com>
2014-08-09 17:27:39 +0100

    add make pics code

---
+++ RpiJavaProject/src/cam/HouseConstructionMonitor.java
@@ -0,0 +1,40 @@
+package cam;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class HouseConstructionMonitor {
+
+	private static Process p;
+	private static BufferedWriter bw;
+	
+	public static void main(String[] args) {
+		HouseConstructionMonitor monitor = new HouseConstructionMonitor();
+		monitor.run();
+	}
+	
+	public void run(){
+		
+		int counter = 1;
+		while(true){
+			try {
+				ProcessBuilder pb = new ProcessBuilder("raspistill -o image"+ ++counter+".jpg");				
+				pb.directory(new File("/leo/cam/test"));
+				
+				pb.redirectErrorStream(true);
+				p = pb.start();
+				OutputStream out = p.getOutputStream();
+				bw = new BufferedWriter(new OutputStreamWriter(out));
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
