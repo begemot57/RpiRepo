9499aab5f4025fe509830c62e8b1b0668e49ae2c
begemot57 <begemot57@hotmail.com>
2014-08-10 13:33:13 +0100

    add monitoring code and javamail lib

--- RpiJavaProject/src/cam/HouseConstructionMonitor.java
+++ RpiJavaProject/src/cam/HouseConstructionMonitor.java
@@ -8,6 +8,10 @@ import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import utils.EmailAttachmentSender;
 import utils.ZipUtils;
 
 /**
@@ -22,15 +26,31 @@ public class HouseConstructionMonitor {
 
 	private static Process p;
 	private static BufferedWriter bw;
-	 private static String picsDir = "/leo/cam/housemonitor/";
+	 private static final String picsDir = "/leo/cam/housemonitor/";
 //	private static String picsDir = "/Users/Leo/git/JavaRepo/JavaTestProject/files/";
 	private static Calendar cal;
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat(
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"yyyy.MM.dd");
-	private static SimpleDateFormat timeFormat = new SimpleDateFormat(
+	private static final SimpleDateFormat timeFormat = new SimpleDateFormat(
 			"HH:mm:ss");
-	private static int[] workingHours = new int[] { 8, 9, 10, 11, 12, 13, 14,
-			15, 16, 17, 18, 22};
+	private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat(
+			"E yyyy.MM.dd HH:mm:ss");
+//	private static final int[] workingHours = new int[] { 8, 9, 10, 11, 12, 13, 14,
+//			15, 16, 17, 18, 22};
+	
+	private static final int[] workingHours = new int[] {13};
+	
+	// SMTP info
+	private static final String host = "smtp.gmail.com";
+	private static final String port = "587";
+	private static final String mailFrom = "leoio1953@gmail.com";
+	private static final String password = "passw0rd1953";
+    // message info
+	private static final String mailTo = "begemot57@hotmail.com";
+	private static final String subject = "House monitor pics: ";
+	private static final String message = "Today's pics are attached.";
+    // attachments
+	private static String[] attachFiles = new String[1];
 	
 	public static void main(String[] args) {
 		HouseConstructionMonitor monitor = new HouseConstructionMonitor();
@@ -46,6 +66,7 @@ public class HouseConstructionMonitor {
 				File todaysDir = new File(picsDir + todaysDirName);
 				if (!todaysDir.exists()) {
 					todaysDir.mkdir();
+					System.out.println("Created today's dir: "+todaysDir.getAbsolutePath());
 				}
 				// check if pic for current hour is there already or it's bad
 				// time to take pic now,
@@ -57,12 +78,14 @@ public class HouseConstructionMonitor {
 				for (File file : filesInFolder) {
 					file.getName().contains("pic" + hoursNow);
 					picAlreadyPresent = true;
+					System.out.println("Pic for current hour is already present: "+file.getName());
 					break;
 				}
 				if (!picAlreadyPresent) {
 					for (int h : workingHours) {
 						if (hoursNow == h) {
 							takePic = true;
+							System.out.println("Current hour is working hour: "+hoursNow);
 							break;
 						}
 					}
@@ -76,20 +99,37 @@ public class HouseConstructionMonitor {
 					p = pb.start();
 					OutputStream out = p.getOutputStream();
 					bw = new BufferedWriter(new OutputStreamWriter(out));
+					System.out.println("Taking pic: "+picName + ".jpg");
 				}
 				
 				//zip up all today's pics and send via e-mail
-				String zipFileName = todaysDirName+".zip";
-				ZipUtils appZip = new ZipUtils();
-				appZip.generateFileList(new File(todaysDir.getAbsolutePath()));
-				appZip.zipIt(todaysDir.getAbsolutePath()+"/"+zipFileName);
-				
-				Thread.sleep(30000);
+				//need to sleep first before last taken pic goes in
+				if(hoursNow==workingHours[workingHours.length-1]){
+					Thread.sleep(10000);
+					String zipFileName = todaysDirName+".zip";
+					ZipUtils appZip = new ZipUtils();
+					appZip.generateFileList(new File(todaysDir.getAbsolutePath()));
+					String pathToZip = todaysDir.getAbsolutePath()+"/"+zipFileName;
+					appZip.zipIt(pathToZip);
+					System.out.println("Created zip file with pics: "+pathToZip);
+			        attachFiles[0] = pathToZip;
+					EmailAttachmentSender.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+					        subject+todaysDirName, message, attachFiles);
+					System.out.println("Email sent to "+mailTo);
+				}
+				//sleep for one hour
+				cal = Calendar.getInstance();
+				System.out.println("Go to sleep for one hour at: "+dateTimeFormat.format(cal.getTime()));
+				Thread.sleep(3600000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (AddressException e) {
+				e.printStackTrace();
+			} catch (MessagingException e) {
 				e.printStackTrace();
 			}
 		}
