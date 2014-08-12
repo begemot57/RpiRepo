11e525533779e47c1603b0ece88b5b8656e74d45
begemot57 <begemot57@hotmail.com>
2014-08-12 21:08:11 +0100

    fix logs

--- RpiJavaProject/src/cam/HouseConstructionMonitor.java
+++ RpiJavaProject/src/cam/HouseConstructionMonitor.java
@@ -27,7 +27,7 @@ public class HouseConstructionMonitor {
 
 	private static Process p;
 	private static BufferedWriter bw;
-	 private static final String picsDir = "/leo/cam/housemonitor/";
+	private static final String picsDir = "/leo/cam/housemonitor/";
 	private static Calendar cal;
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"yyyy.MM.dd");
@@ -35,43 +35,45 @@ public class HouseConstructionMonitor {
 			"HH:mm:ss");
 	private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat(
 			"E yyyy.MM.dd 'at' HH:mm:ss");
-	private static final int[] workingHours = new int[] { 7, 8, 9, 10, 11, 12, 13, 14,
-			15, 16, 17};
-	
+	private static final int[] workingHours = new int[] { 7, 8, 9, 10, 11, 12,
+			13, 14, 15, 16, 17 };
+
 	// SMTP info
 	private static final String host = "smtp.gmail.com";
 	private static final String port = "587";
 	private static final String mailFrom = "leoio1953@gmail.com";
 	private static final String password = "passw0rd1953";
-    // message info
+	// message info
 	private static final String mailTo = "begemot57@hotmail.com";
 	private static final String subject = "House monitor pics: ";
 	private static final String message = "Today's pics are attached.";
-    // attachments
+	// attachments
 	private static String[] attachFiles = new String[1];
-	//out stream
+	// out stream
 	private static PrintWriter out;
-	
+
 	public static void main(String[] args) {
 		HouseConstructionMonitor monitor = new HouseConstructionMonitor();
 		monitor.run();
 	}
 
 	public void run() {
-		while (true) {
-			try {
-				//setup out stream
-				cal = Calendar.getInstance();
-				String log_file_name = dateTimeFormat.format(cal.getTime()).replace(" ", "").concat(".log");
-				out = new PrintWriter(new File(log_file_name));
-				
+		try {
+			// setup out stream
+			cal = Calendar.getInstance();
+			String log_file_name = dateTimeFormat.format(cal.getTime())
+					.replace(" ", "").concat(".log");
+			out = new PrintWriter(new File(log_file_name));
+			while (true) {
+
 				// check if today's pic dir is there or create one
 				cal = Calendar.getInstance();
 				String todaysDirName = dateFormat.format(cal.getTime());
 				File todaysDir = new File(picsDir + todaysDirName);
 				if (!todaysDir.exists()) {
 					todaysDir.mkdir();
-					out.write("Created today's dir: "+todaysDir.getAbsolutePath()+"\n");
+					out.write("Created today's dir: "
+							+ todaysDir.getAbsolutePath() + "\n");
 					out.flush();
 				}
 				// check if pic for current hour is there already or it's bad
@@ -82,9 +84,10 @@ public class HouseConstructionMonitor {
 				boolean takePic = false;
 				boolean picAlreadyPresent = false;
 				for (File file : filesInFolder) {
-					if(file.getName().contains("pic" + hoursNow)){
+					if (file.getName().contains("pic" + hoursNow)) {
 						picAlreadyPresent = true;
-						out.write("Pic for current hour is already present: "+file.getName()+"\n");
+						out.write("Pic for current hour is already present: "
+								+ file.getName() + "\n");
 						out.flush();
 						break;
 					}
@@ -93,7 +96,8 @@ public class HouseConstructionMonitor {
 					for (int h : workingHours) {
 						if (hoursNow == h) {
 							takePic = true;
-							out.write("Current hour is working hour: "+hoursNow+"\n");
+							out.write("Current hour is working hour: "
+									+ hoursNow + "\n");
 							out.flush();
 							break;
 						}
@@ -101,55 +105,58 @@ public class HouseConstructionMonitor {
 				}
 				if (takePic) {
 					String picName = "pic" + timeFormat.format(cal.getTime());
-					ProcessBuilder pb = new ProcessBuilder(
-							"raspistill", "-w", "800", "-h", "600", "-o", picName + ".jpg");
+					ProcessBuilder pb = new ProcessBuilder("raspistill", "-w",
+							"800", "-h", "600", "-o", picName + ".jpg");
 					pb.directory(new File(todaysDir.getAbsolutePath()));
 					pb.redirectErrorStream(true);
 					p = pb.start();
 					OutputStream outStream = p.getOutputStream();
 					bw = new BufferedWriter(new OutputStreamWriter(outStream));
-					out.write("Taking pic: "+picName + ".jpg"+"\n");
+					out.write("Taking pic: " + picName + ".jpg" + "\n");
 					out.flush();
 				}
-				
-				//zip up all today's pics and send via e-mail
-				//need to sleep first before last taken pic goes in
-				if(hoursNow==workingHours[workingHours.length-1]){
+
+				// zip up all today's pics and send via e-mail
+				// need to sleep first before last taken pic goes in
+				if (hoursNow == workingHours[workingHours.length - 1]) {
 					Thread.sleep(10000);
-					String zipFileName = todaysDirName+".zip";
-					String pathToZip = picsDir+zipFileName;
+					String zipFileName = todaysDirName + ".zip";
+					String pathToZip = picsDir + zipFileName;
 					ZipUtils.createZip(pathToZip, todaysDir.getAbsolutePath());
-					out.write("Created zip file with pics: "+pathToZip+"\n");
+					out.write("Created zip file with pics: " + pathToZip + "\n");
 					out.flush();
-			        attachFiles[0] = pathToZip;
-					EmailAttachmentSender.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
-					        subject+todaysDirName, message, attachFiles);
-					out.write("Email sent to "+mailTo+"\n");
+					attachFiles[0] = pathToZip;
+					EmailAttachmentSender.sendEmailWithAttachments(host, port,
+							mailFrom, password, mailTo,
+							subject + todaysDirName, message, attachFiles);
+					out.write("Email sent to " + mailTo + "\n");
 					out.flush();
 				}
-				//sleep for one hour
+				// sleep for one hour
 				cal = Calendar.getInstance();
-				out.write("Go to sleep for one hour at: "+dateTimeFormat.format(cal.getTime())+"\n");
+				out.write("Go to sleep for one hour at: "
+						+ dateTimeFormat.format(cal.getTime()) + "\n");
 				out.flush();
 				Thread.sleep(3600000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace(out);
-			} catch (SecurityException e) {
-				e.printStackTrace(out);
-			} catch (AddressException e) {
-				e.printStackTrace(out);
-			} catch (MessagingException e) {
-				e.printStackTrace(out);
-			} finally {
-				if (out != null) {
-					out.write("Close file");
-					out.flush();
-					out.close();
-				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace(out);
+		} catch (SecurityException e) {
+			e.printStackTrace(out);
+		} catch (AddressException e) {
+			e.printStackTrace(out);
+		} catch (MessagingException e) {
+			e.printStackTrace(out);
+		} finally {
+			if (out != null) {
+				out.write("Close file");
+				out.flush();
+				out.close();
 			}
 		}
+
 	}
 
 }
