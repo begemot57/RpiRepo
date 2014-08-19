f7562fe8ebb1ec1711d1418e406ad07efd8408f1
begemot57 <begemot57@hotmail.com>
2014-08-19 21:45:34 +0100

    redirect output to null

--- RpiJavaProject/scripts/cam_and_mjpg_streamer.sh
+++ RpiJavaProject/scripts/cam_and_mjpg_streamer.sh
@@ -7,10 +7,10 @@ function start(){
 	rm /leo/cam/picstream/*
 	
 	echo "start raspistill into /leo/cam/picstream/pic.jpg"
-	raspistill --nopreview -w 640 -h 480 -q 5 -o /leo/cam/picstream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
+	raspistill --nopreview -w 640 -h 480 -q 5 -o /leo/cam/picstream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 > /dev/null 2>&1 &
 	
 	echo "start mjpg_streamer"
-	LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /leo/cam/picstream -n pic.jpg" -o "output_http.so -w /usr/local/www -p 8090" &
+	LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /leo/cam/picstream -n pic.jpg" -o "output_http.so -w /usr/local/www -p 8090" > /dev/null 2>&1 &
 }
 
 function stop(){
