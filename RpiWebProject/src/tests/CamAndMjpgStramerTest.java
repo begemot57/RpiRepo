63ae4d74982341fc84e15909ee4ff3e970393cb4
begemot57 <begemot57@hotmail.com>
2014-08-20 15:27:12 +0100

    add test

---
+++ RpiWebProject/src/tests/CamAndMjpgStramerTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import beans.CamAndMjpgStramer;
+
+public class CamAndMjpgStramerTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CamAndMjpgStramer streamer = new CamAndMjpgStramer();
+        streamer.start();
+	}
+
+}
