faedec42fa529d38a04d8ec03b751c9ec14e55e1
begemot57 <begemot57@hotmail.com>
2014-08-26 00:03:00 +0100

    fs

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#sudo su <<HERE
-#pwdvar=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -1)
-#echo $pwdvar
+
+sudo su <<HERE
+
 currentlogfile=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -t * | head -1)
 echo $currentlogfile
 #line=$(head -n 1 $currentlogfile)
 #index=$(echo $line | grep -b -o @ | cut -d: -f1)
 #echo $index
 #echo ${line:0:$index}
-#HERE
+exit
+
+HERE
