ec839d21efc34ffa822d804ddcb4a760d90f3253
begemot57 <begemot57@hotmail.com>
2014-08-25 23:12:49 +0100

    fix script

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 line=$(head -n 1 Configuration.conf)
-echo $line | grep -b -o @ | cut -d: -f1
+echo echo $line | grep -b -o @ | cut -d: -f1
+index=$(echo $line | grep -b -o @ | cut -d: -f1)
+echo $index
 echo ${line:0:4}
