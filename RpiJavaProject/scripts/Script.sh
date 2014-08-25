fcf88fa084516962d35ccda816d695c08224825e
begemot57 <begemot57@hotmail.com>
2014-08-25 23:41:31 +0100

    fs

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -2,8 +2,8 @@
 sudo su <<HERE
 currentlogfile=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -t *.log | head -1)
 echo $currentlogfile
-line=$(head -n 1 $currentlogfile)
-index=$(echo $line | grep -b -o @ | cut -d: -f1)
-echo $index
-echo ${line:0:$index}
+#line=$(head -n 1 $currentlogfile)
+#index=$(echo $line | grep -b -o @ | cut -d: -f1)
+#echo $index
+#echo ${line:0:$index}
 HERE
