27c2f8b8aff425a7ee030095a3ccf428910a74a7
begemot57 <begemot57@hotmail.com>
2014-08-25 16:18:20 +0100

    fix script

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -1,5 +1,9 @@
-source Configuration.conf
-
-echo $Name
-echo $Age
-echo $Address
+#!/bin/bash
+OLDIFS=$IFS
+IFS="/"
+while read f1 f2
+do
+	echo "process id : $f1"
+	echo "host : $f2"
+done < file
+IFS=$OLDIFS
