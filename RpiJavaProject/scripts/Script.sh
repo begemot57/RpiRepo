e6851cd1cf53652998178f9ac9831cb9e9d09a33
root <root@raspberrypi.(none)>
2014-08-26 00:00:36 +0100

    fix

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo su <<HERE
+#sudo su <<HERE
+#pwdvar=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -1)
+#echo $pwdvar
 currentlogfile=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -t * | head -1)
 echo $currentlogfile
 #line=$(head -n 1 $currentlogfile)
 #index=$(echo $line | grep -b -o @ | cut -d: -f1)
 #echo $index
 #echo ${line:0:$index}
-HERE
+#HERE
