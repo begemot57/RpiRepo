a74896138ea1990820764ddb59e11f60ccedb05d
begemot57 <begemot57@hotmail.com>
2014-08-25 23:38:49 +0100

    fix scr

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -2,7 +2,7 @@
 sudo su <<HERE
 currentlogfile=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -t *.log | head -1)
 echo $currentlogfile
-line=$(head -n 1 Configuration.conf)
+line=$(head -n 1 $currentlogfile)
 index=$(echo $line | grep -b -o @ | cut -d: -f1)
 echo $index
 echo ${line:0:$index}
