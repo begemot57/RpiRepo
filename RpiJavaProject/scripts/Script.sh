f5e7f8272e4a2764b7a4211962a5a9a96961f3c2
begemot57 <begemot57@hotmail.com>
2014-08-25 23:29:33 +0100

    fix script

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /leo/git/RpiRepo/RpiJavaProject/bin
-echo ls -t *.log | head -1
-
+cd /leo/git/RpiRepo/RpiJavaProject/bin &
+currentlogfile=$(ls -t *.log | head -1)
+echo $currentlogfile
 line=$(head -n 1 Configuration.conf)
 index=$(echo $line | grep -b -o @ | cut -d: -f1)
 echo $index
