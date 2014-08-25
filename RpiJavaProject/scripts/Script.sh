9877c52b6b9f5fe40adcb7d46749770dec8e60ca
begemot57 <begemot57@hotmail.com>
2014-08-26 00:04:57 +0100

    fs

--- RpiJavaProject/scripts/Script.sh
+++ RpiJavaProject/scripts/Script.sh
@@ -4,10 +4,10 @@
 
 currentlogfile=$(cd /leo/git/RpiRepo/RpiJavaProject/bin && ls -t * | head -1)
 echo $currentlogfile
-#line=$(head -n 1 $currentlogfile)
-#index=$(echo $line | grep -b -o @ | cut -d: -f1)
-#echo $index
-#echo ${line:0:$index}
+line=$(head -n 1 $currentlogfile)
+index=$(echo $line | grep -b -o @ | cut -d: -f1)
+echo $index
+echo ${line:0:$index}
 exit
 
 #HERE
