b0a30fd33919f6c61f1fd782dc1f4c3b2d63bb77
begemot57 <begemot57@hotmail.com>
2014-08-26 21:27:16 +0100

    fix hm scr

--- RpiJavaProject/scripts/housemonitoring.sh
+++ RpiJavaProject/scripts/housemonitoring.sh
@@ -6,39 +6,37 @@
 #sudo su <<HERE
 
 function start(){
-	echo "starting house monitoring process"
+	echo "Starting house monitoring process"
 	path="/leo/git/RpiRepo/RpiJavaProject/bin/"
 	currentlogfile=$(sudo ls -t $path | head -1)
-	echo $path$currentlogfile
+	echo "Get PID from here: "$path$currentlogfile
 	line=$(head -n 1 $path$currentlogfile)
 	index=$(echo $line | grep -b -o @ | cut -d: -f1)
-	echo $index
 	PID=${line:0:$index}
-	echo $PID
+	echo "PID is:"$PID
 	if ps -p $PID > /dev/null; then
-   		echo "$PID is running, no need to start"
+   		echo "Process $PID is running, no need to start"
    	else
-   		echo "no $PID running now - gonna start"	
+   		echo "No process $PID is running now - gonna start new one"	
    		cd /leo/git/RpiRepo/RpiJavaProject/bin
 		sudo java -cp .:../lib/java-mail-1.4.jar cam.HouseConstructionMonitor &
 	fi
 }
 
 function stop(){
-	echo "killing house monitoring process"
+	echo "Killing house monitoring process"
 	path="/leo/git/RpiRepo/RpiJavaProject/bin/"
 	currentlogfile=$(sudo ls -t $path | head -1)
-	echo $path$currentlogfile
+	echo "Get PID from here: "$path$currentlogfile
 	line=$(head -n 1 $path$currentlogfile)
 	index=$(echo $line | grep -b -o @ | cut -d: -f1)
-	echo $index
 	PID=${line:0:$index}
-	echo $PID
+	echo "PID is:"$PID
 	if ps -p $PID > /dev/null; then
-   		echo "$PID is running"
-   		sudo kill -9 $PID
+   		echo "Process $PID is running - gonna kill it now"
+   		sudo kill $PID
    	else
-   		echo "no $PID running now"	
+   		echo "No process $PID is running now - nothing to kill"
 	fi		
 
 }
