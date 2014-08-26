f931bd8f904be69f940067cd6715c29708ba5b3b
begemot57 <begemot57@hotmail.com>
2014-08-26 23:19:11 +0100

    add log to house monitor java process to see what the bash script is saying

--- RpiWebProject/src/beans/HouseMonitor.java
+++ RpiWebProject/src/beans/HouseMonitor.java
@@ -1,11 +1,12 @@
 package beans;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class HouseMonitor {
@@ -16,13 +17,13 @@ public class HouseMonitor {
 	private static final String scriptName = "./housemonitor.sh";
 	// out stream
 	private static PrintWriter out;
-	private static File logFile = new File("streamer.log");
+	private static File logFile = new File("housemonitor.log");
 	
 	public HouseMonitor(){
 		if(pb == null){
 			try {
 				out = new PrintWriter(logFile);
-				out.write("CamAndMjpgStreamer initialized\n");
+				out.write("Init house moinitor object\n");
 				out.flush();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace(out);
@@ -32,17 +33,18 @@ public class HouseMonitor {
 	}
 	
 	public void start() {
-		out.write("start pic stream and server\n");
+		out.write("Detected Start button click\n");
 		out.flush();
 		execute("start");
 	}
 	
 	public void stop(){
-		out.write("stop pic stream and server\n");
+		out.write("Detected Stop button click\n");
 		execute("stop");
 	}
 
 	void execute(String action){
+		BufferedWriter bufWriter = null;
 		try {
 			if(bw == null){
 				pb = new ProcessBuilder(scriptName, action);
@@ -50,12 +52,23 @@ public class HouseMonitor {
 				pb.redirectErrorStream(true);
 //				pb.redirectInput(logFile);
 				Process p = pb.start();
-//				OutputStream pbOut = p.getOutputStream();
-//				bw = new BufferedWriter(new OutputStreamWriter(pbOut));
+				InputStream inputStream = p.getInputStream();
+				BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream));
+				bufWriter = new BufferedWriter(out);
+				for (String line = bufReader.readLine(); line != null; line = bufReader.readLine()) {
+					bufWriter.write(line + "\n");
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace(out);
 			out.flush();
-		} 
+		} finally {
+			try {
+				bufWriter.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
