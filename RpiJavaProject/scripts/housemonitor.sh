0819c543734f33012410bc2db6a12aa06ad2f3cb
begemot57 <begemot57@hotmail.com>
2014-08-29 15:36:43 +0100

    add checkstate to housemonitor

--- RpiJavaProject/scripts/housemonitor.sh
+++ RpiJavaProject/scripts/housemonitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This will start/stop house monitoring tool.
-# To run go: ./housemonitor.sh start/stop
+# To run go: ./housemonitor.sh start/stop/checkstate
 
 #this is needed to run script as root, otherwise tomcat7 can't execute it
 #sudo su <<HERE
@@ -38,7 +38,19 @@ function stop(){
    	else
    		echo "No process $PID is running now - nothing to kill"
 	fi		
+}
 
+function checkstate(){
+	path="/leo/git/RpiRepo/RpiJavaProject/bin/"
+	currentlogfile=$(sudo ls -t $path | head -1)
+	line=$(head -n 1 $path$currentlogfile)
+	index=$(echo $line | grep -b -o @ | cut -d: -f1)
+	PID=${line:0:$index}
+	if ps -p $PID > /dev/null; then
+   		echo "running"
+   	else
+   		echo "stopped"
+	fi	
 }
 
 if [ $1 == "start" ]; then
@@ -49,6 +61,10 @@ fi
 if [ $1 == "stop" ]; then
 	echo "stop it"
 	stop
+fi
+
+if [ $1 == "checkstate" ]; then
+	checkstate
 fi
 
 exit
