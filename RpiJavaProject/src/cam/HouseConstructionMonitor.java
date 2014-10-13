d8cbb8f10d52a8477dbc5cab0c6fd42e9992794c
begemot57 <begemot57@hotmail.com>
2014-10-13 23:02:35 +0100

    revert house monitor test to its original state

--- RpiJavaProject/src/cam/HouseConstructionMonitor.java
+++ RpiJavaProject/src/cam/HouseConstructionMonitor.java
@@ -37,7 +37,7 @@ public class HouseConstructionMonitor {
 	private static final SimpleDateFormat numbersOnlyFormat = new SimpleDateFormat(
 			"yyyyMMdd'_'HHmm");
 	private static final int[] workingHours = new int[] { 8, 9, 10, 11, 12, 13,
-			14, 15, 16, 17, 18, 23 };
+			14, 15, 16, 17, 18};
 
 	// SMTP info
 	private static final String host = "smtp.gmail.com";
